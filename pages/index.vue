6cceebbd67c0db245f033d83dcb209b93c16dcf9
jennie.shao <m65pjj@gmail.com>
2021-11-16 20:15:02 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  methods: {
+    add() {
+      const a = '234'
+
+      return a
+    },
+  },
+})
 </script>
