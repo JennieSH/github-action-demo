ca1f48c448c9a1521f0d9135324b2b183e99c6dc
jennie.shao <m65pjj@gmail.com>
2021-11-19 06:13:56 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-![Test](https://github.com/JennieSH/github-action-demo/actions/workflows/main.yml/badge.svg)
+![Test](https://github.com/JennieSH/github-action-demo/actions/workflows/deploy.yml/badge.svg)
 
 # GitHub Action Demo
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/github-action-demo/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'test-nx',
