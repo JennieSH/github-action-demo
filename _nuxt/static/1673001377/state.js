272d31ec4d9c07631d2d8d98f24d6a369d9ce995
JennieSH <56363009+JennieSH@users.noreply.github.com>
2023-01-06 10:44:00 +0000

    Deploying to gh-pages from @ JennieSH/github-action-demo@1da4ed4e59ba09b3f972275dc0f5b59017343d81 🚀

--- _nuxt/static/1673001377/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgithub-action-demo\u002F_nuxt\u002Fstatic\u002F1673001377",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fgithub-action-demo\u002F",assetsPath:"\u002Fgithub-action-demo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
